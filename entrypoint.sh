57a5660fcb4389042e8ab3304c9123f88cb4e704
Brandon Powell <4072417+brandon-powell@users.noreply.github.com>
2020-04-20 23:54:29 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,13 @@
 cd "$GITHUB_WORKSPACE"
 
 git fetch
-echo $(git --version)
-echo $(clang-format --version)
-diff=`git-clang-format --diff --commit $INPUT_BASE`
-[ "$diff" = "no modified files to format" ] && exit 0
-[ "$diff" = "clang-format did not modify any files" ] && exit 0
+git --version
+clang-format --version
+#diff=`git-clang-format --diff --commit $INPUT_BASE`
+#[ "$diff" = "no modified files to format" ] && exit 0
+#[ "$diff" = "clang-format did not modify any files" ] && exit 0
 
-printf "\033[1mYou have introduced coding style breakages, suggested changes:\n\n"
+#printf "\033[1mYou have introduced coding style breakages, suggested changes:\n\n"
 
-echo "$diff" | colordiff
+#echo "$diff" | colordiff
 exit 1
