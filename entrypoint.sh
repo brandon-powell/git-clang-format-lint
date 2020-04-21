0e5e4a0bd14ca898accaeb4704e5cf0e347e4c32
Brandon Powell <4072417+brandon-powell@users.noreply.github.com>
2020-04-20 23:50:37 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@
 cd "$GITHUB_WORKSPACE"
 
 git fetch
+echo $(git --version)
+echo $(clang-format --version)
 diff=`git-clang-format --diff --commit $INPUT_BASE`
 [ "$diff" = "no modified files to format" ] && exit 0
 [ "$diff" = "clang-format did not modify any files" ] && exit 0
